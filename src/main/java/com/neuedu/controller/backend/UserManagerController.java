9b56796d9ef297e43ea8e6158f48eb0aaa820c3f
gy <3137772593@qq.com>
2018-11-26 09:44:23 +0800

    redis封装

--- src/main/java/com/neuedu/controller/backend/UserManagerController.java
+++ src/main/java/com/neuedu/controller/backend/UserManagerController.java
@@ -1,6 +1,8 @@
 package com.neuedu.controller.backend;
 
 import com.neuedu.common.ServerResponse;
+import com.neuedu.pojo.UserInfo;
+import com.neuedu.redis.RedisService;
 import com.neuedu.service.ICategoryService;
 import com.neuedu.service.IUserService;
 import lombok.extern.slf4j.Slf4j;
@@ -26,4 +28,53 @@ public class UserManagerController {
         log.error("=========add===controller==");
         return null;
     }
+
+    @Autowired
+    RedisService redisService;
+    @RequestMapping(value = "set.do")
+    public ServerResponse set(){
+        log.error("=========add===controller==");
+        UserInfo userInfo=new UserInfo();
+        userInfo.setId(1);
+        userInfo.setUsername("张三");
+        redisService.set("user:1:info",userInfo);
+
+        return ServerResponse.createServerResponseBySuccess(userInfo);
+    }
+    @RequestMapping(value = "get.do")
+    public ServerResponse get(){
+        log.error("=========add===controller==");
+
+        UserInfo userInfo=redisService.get("user:1:info",UserInfo.class);
+        System.out.println(userInfo.getUsername());
+        return ServerResponse.createServerResponseBySuccess(userInfo);
+
+    }
+
+    @RequestMapping(value = "setex.do")
+    public ServerResponse setex(){
+        log.error("=========add===controller==");
+        UserInfo userInfo=new UserInfo();
+        userInfo.setId(1);
+        userInfo.setUsername("张三");
+        redisService.setex("user:1:info",userInfo,100);
+
+        return ServerResponse.createServerResponseBySuccess(userInfo);
+    }
+
+    @RequestMapping(value = "del.do")
+    public ServerResponse del(){
+        log.error("=========add===controller==");
+        long re=redisService.del("user:1:info");
+
+        return ServerResponse.createServerResponseBySuccess(re);
+    }
+    @RequestMapping(value = "expire.do")
+    public ServerResponse expire(){
+        log.error("=========add===controller==");
+        long re=redisService.expire("user:1:info",110);
+
+        return ServerResponse.createServerResponseBySuccess(re);
+    }
+
 }
